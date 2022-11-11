3eb1aa52dd77d2b95c1a1a0c170baf070c4095c6
meir shtiglitz <m.stigel@gmail.com>
2022-11-11 09:56:58 +0200

    add cors option

--- server/index.js
+++ server/index.js
@@ -26,7 +26,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 const upload = multer({ dest: 'uploads/' });
 
-app.use(cors());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+app.use(cors(corsOptions));
 app.use('/api', routeUser);
 app.use('/api', routeNLetter);
 app.use('/api', routeContact);
