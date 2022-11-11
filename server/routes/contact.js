f5963fbbe8e6202780042c6eedd031f2ecda9de5
meir shtiglitz <m.stigel@gmail.com>
2022-11-11 10:11:58 +0200

    add cors allowed to contact api

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -4,6 +4,9 @@ const { sendMail } = require('../model/emails');
 
 router.post("/contact/sendmail", async(req, res) => {
     console.log("req.body from route contact",req.body);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authortization');
+    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
     const {from, msg} = req.body;
     const result = await sendMail("contact msg", process.env.ADMIN_MAIL,`from ${from}`, `<p>the msg: ${msg} </p>`);
     console.log(result)
